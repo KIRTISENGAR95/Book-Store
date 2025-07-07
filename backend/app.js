ef53df91a15da1ccbdb623f6f056ab040499e1ed
kirti sengar <kirtisengar95@gmail.com>
2025-07-07 18:08:41 +0530

    commit

--- backend/app.js
+++ backend/app.js
@@ -11,9 +11,17 @@ app.use(express.json());
 const user = require("./routes/user");
 const Books = require("./routes/book");
 const Favourite = require("./routes/favourite");
+const Cart = require("./routes/cart");
+const Order = require("./routes/order");
+
+
+
 app.use("/api/v1",user);
 app.use("/api/v1",Books);
 app.use("/api/v1",Favourite);
+app.use("/api/v1",Cart);
+app.use("/api/v1",Order);
+
 
 console.log(process.env.PORT)
 const PORT = process.env.PORT || 3000;                      
